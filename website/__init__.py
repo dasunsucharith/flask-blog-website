58e3541efe43c5ec0fdc98828c950d83cf77eb67
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-07-11 15:32:47 +0530

    new

--- website/__init__.py
+++ website/__init__.py
@@ -3,14 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "slviki.org"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views 
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    
 
     return app
