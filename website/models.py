7a8ca3fe2dcc0151698182e92f459b6086ac4d5e
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-07-12 20:28:02 +0530

    new

--- website/models.py
+++ website/models.py
@@ -10,6 +10,7 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,14 +19,22 @@ class Post(db.Model):
     author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete='CASCADE'), nullable=False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200), nullable=True)
-    date_created = db.Column(db.Datetime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.Foreignkey(
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)
+
 
 
 
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Post, User, Comment
+from .models import Post, User, Comment, Like
 from . import db
 
 views = Blueprint("views", __name__)
@@ -10,7 +10,10 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     posts = Post.query.all()
-    return render_template('home.html', name=current_user, posts=posts)
+    return render_template('home.html', user=current_user, posts=posts)
+
+
+
 
 @views.route('/create-post', methods=['GET', 'POST'])
 @login_required
@@ -29,6 +32,9 @@ def create_post():
 
     return render_template('create_post.html', user=current_user)
 
+
+
+
 @views.route('/delete-post/<id>')
 @login_required
 def delete_post(id):
@@ -45,6 +51,9 @@ def delete_post(id):
 
     return redirect(url_for('views.home'))
 
+
+
+
 @views.route('/posts/<username>')
 @login_required
 def posts(username):
@@ -56,6 +65,9 @@ def posts(username):
 
     posts = user.posts
     return render_template('posts.html',user=current_user, posts=posts, username=username)
+
+
+
 
 @views.route("/create-comment/<post_id>", methods=['POST'])
 @login_required
@@ -75,6 +87,9 @@ def create_comment(post_id):
     
     return redirect(url_for('views.home'))
 
+
+
+
 @views.route("/delete-comment/<comment_id>")
 @login_required
 def delete_comment(comment_id):
@@ -89,4 +104,24 @@ def delete_comment(comment_id):
         db.session.commit()
 
     return redirect(url_for('views.home'))
+
+
+
+@views.route("/like-post/<post_id>", methods=['POST'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        return jsonify({'error': 'Post does not exist.'}, 400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+
+    return jsonify({'likes': len(post.likes), 'liked': current_user.id in map(lambda x: x.author, post.likes)})
 
